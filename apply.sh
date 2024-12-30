530f480d38c98e30a0f6fc1a258b5b395b7d027a
Vitaly Merenkov <vmrnkv@gmail.com>
2024-12-30 14:33:00 +0300

    feat: add apply script

---
+++ apply.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+mkdir -p ~/.tmux/plugins/tpm
+mkdir -p ~/.config/alacritty/themes
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
+
+ln -s $(pwd)/tmux.conf ~/.tmux.conf
+ln -s $(pwd)/zshrc ~/.zshrc
+ln -s $(pwd)/alacritty.toml ~/.config/alacritty/alacritty.toml
